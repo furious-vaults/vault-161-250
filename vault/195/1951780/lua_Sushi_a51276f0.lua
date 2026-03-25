16c68aa0fa61324d569cf2887abb832814550d35
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 09:19:06 -0300

    Upload Lote 1 - Prefixo 195

---
+++ vault/195/1951780/lua_Sushi_a51276f0.lua
@@ -0,0 +1,19 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(1951780) -- Jumping Jazz Cats
+
+-- MAIN APP DEPOTS
+addappid(1951781, 1, "1a2665d2ccc26b6fc849acae18c8932f9d802794a174a7eeaa8addb988e0c1f7") -- Main Game Content (Windows Content)
+setManifestid(1951781, "3967857811879165383", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS EXCLUDED (NO DEPOT KEYS, NO DEDICATED DEPOTS)
+-- addappid(3558450) -- Jumping Jazz Cats - Soundtrack (no keys available)
