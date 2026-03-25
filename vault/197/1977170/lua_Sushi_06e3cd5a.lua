c4e481af5583e90ecc42d7af69f6600c5db1d8d9
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 09:21:08 -0300

    Upload Lote 1 - Prefixo 197

---
+++ vault/197/1977170/lua_Sushi_06e3cd5a.lua
@@ -0,0 +1,24 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(1977170) -- Jusant
+
+-- MAIN APP DEPOTS
+addappid(1977171, 1, "499a8c9d6e61ef12de56cfc87af782e9e635a83119608889231331bd8d244655") -- Main Game Content (Windows Content)
+setManifestid(1977171, "7104167019741821837", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- Jusant Original Soundtrack (AppID: 2645290)
+addappid(2645290)
+addappid(2645291, 1, "65b29d3ee6a2e6ee5be7e3f9656900f8271113f9794d078d3e11da5271cd7623") -- Jusant Original Soundtrack - Main Content
+setManifestid(2645291, "3354880252151546124", 0)
+addappid(2645292, 1, "aed6dd0410352ccd366c8f4e7cf10fee4a6dbcb879745c7e44749cc178db5532") -- Jusant Original Soundtrack - Main Content
+setManifestid(2645292, "396568433112906612", 0)
