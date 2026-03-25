3e5cf2c2c6830597a6bfbfce6ea3856129ee7aea
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 09:29:30 -0300

    Upload Lote 1 - Prefixo 205

---
+++ vault/205/2050650/lua_Sushi_b6fa0ebc.lua
@@ -0,0 +1,158 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(2050650) -- Resident Evil 4
+
+-- MAIN APP DEPOTS
+addappid(2050651, 1, "c3bc4be6ae2c98c50f2ba78469906e43be35b408daf8d49f420aaf6cad275b2b") -- Main Game Content (Windows Content)
+setManifestid(2050651, "542357299209751671", 0)
+addappid(2050652, 1, "56b9ebbf0f22a11346e420e85c5b9d7e252e3981b4887aa0511bb237c8b2331c") -- Game Content (Linux Binaries)
+setManifestid(2050652, "6176917678100737873", 0)
+addappid(2050653, 1, "219060cb9daf307498b27b009af1468282599d2d3fdf2b23729c0bd833b84053") -- Game Content (Mac Content)
+setManifestid(2050653, "7108738021112316256", 0)
+addappid(2050654, 1, "cca460bcec8cc6fecefd24495f686004d921114b58aa3869f6832eb5f5e2612c") -- Common Library Files (Content)
+setManifestid(2050654, "4152569296804840016", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- Resident Evil 4 - Separate Ways (AppID: 2109300)
+addappid(2109300)
+addappid(2109300, 1, "e15a644e1234ac8a62f6bd4e1ac16dcd5ee651810325af841f45abeb13ea3121") -- Resident Evil 4 - Separate Ways - Main Content
+setManifestid(2109300, "5618180671040197314", 0)
+addappid(2050655, 1, "a29070fea49c350b8bd9b7f5841f56f28bc7537343475dd5d23d64dc32cea3d6") -- Resident Evil 4 - Separate Ways - DLC Content
+setManifestid(2050655, "8334430982901761350", 0)
+
+-- Resident Evil 4 Treasure Map Expansion (AppID: 2109301)
+addappid(2109301)
+addappid(2109301, 1, "d2ec1b74333218e11dc8471f4e2057fd825d407a4c47dfc646df60881a5841b9") -- Resident Evil 4 Treasure Map Expansion - Main Content
+setManifestid(2109301, "360070119915724610", 0)
+
+-- Resident Evil 4 Leon  Ashley Costumes Casual (AppID: 2109303)
+addappid(2109303)
+addappid(2109303, 1, "5bf57eede21276d7dbab353eb7272c6e6a7ffca40fd535337f8b64dc1f7df1c0") -- Resident Evil 4 Leon  Ashley Costumes Casual - Main Content
+setManifestid(2109303, "9166301326971889287", 0)
+
+-- Resident Evil 4 Leon  Ashley Costumes Romantic (AppID: 2109304)
+addappid(2109304)
+addappid(2109304, 1, "3d8963f6736172da45ac32e19df3078f2cb2679f4a34cb0b70970e183241ebd3") -- Resident Evil 4 Leon  Ashley Costumes Romantic - Main Content
+setManifestid(2109304, "382218590876545051", 0)
+
+-- Resident Evil 4 Leon Costume  Filter Hero (AppID: 2109305)
+addappid(2109305)
+addappid(2109305, 1, "7df756b7ea77cd773635eaba56572deb90e4518e52b0620d47c9cc8ed10b1798") -- Resident Evil 4 Leon Costume  Filter Hero - Main Content
+setManifestid(2109305, "8874053015552356169", 0)
+
+-- Resident Evil 4 Leon Costume  Filter Villain (AppID: 2109306)
+addappid(2109306)
+addappid(2109306, 1, "5b1eb1787a6d0500e6cb23a33b19e593c998a07341c04525c9aa0e608d4796ca") -- Resident Evil 4 Leon Costume  Filter Villain - Main Content
+setManifestid(2109306, "6308526370650425368", 0)
+
+-- Resident Evil 4 Original Ver. Soundtrack Swap (AppID: 2109307)
+addappid(2109307)
+addappid(2109307, 1, "cec2c4f51874f8b5d2dc936e3514bc3c6ce3cd790a609e711d1b5be1fc1159d4") -- Resident Evil 4 Original Ver. Soundtrack Swap - Main Content
+setManifestid(2109307, "5310837729215635277", 0)
+
+-- Resident Evil 4 Deluxe Weapon Sentinel Nine (AppID: 2109308)
+addappid(2109308)
+addappid(2109308, 1, "cae75fb7131ee6918d86a2ba6b4195e729f93fc11291da373fe66dcfe72856ee") -- Resident Evil 4 Deluxe Weapon Sentinel Nine - Main Content
+setManifestid(2109308, "964709411673088664", 0)
+
+-- Resident Evil 4 Deluxe Weapon Skull Shaker (AppID: 2109309)
+addappid(2109309)
+addappid(2109309, 1, "8e04a74054c1ecc380723dbed552ab381ea28d8abfef53011f2c42e4989594f9") -- Resident Evil 4 Deluxe Weapon Skull Shaker - Main Content
+setManifestid(2109309, "2339425891140016912", 0)
+
+-- Resident Evil 4 Leon Accessory Sunglasses (Sporty) (AppID: 2109310)
+addappid(2109310)
+addappid(2109310, 1, "75ed1eef5aa69f5795bf6d3a5f859964fcbbceb43e3fd401f12f52c1b2801b30") -- Resident Evil 4 Leon Accessory Sunglasses (Sporty) - Main Content
+setManifestid(2109310, "6423275757951298947", 0)
+
+-- Resident Evil 4 Attach Case Gold (AppID: 2109311)
+addappid(2109311)
+addappid(2109311, 1, "98e7b894d2d5d437f4f5572291055db7dcc846b46381a0f439884cf45ed763e0") -- Resident Evil 4 Attach Case Gold - Main Content
+setManifestid(2109311, "2476102622028354069", 0)
+
+-- Resident Evil 4 Attach Case Classic (AppID: 2109312)
+addappid(2109312)
+addappid(2109312, 1, "ca6584421429d6954970bff9838104fabace1d7bbdd465ddc1ed69a829b8e6ff") -- Resident Evil 4 Attach Case Classic - Main Content
+setManifestid(2109312, "7506742638748304084", 0)
+
+-- Resident Evil 4 Charm Handgun Ammo (AppID: 2109313)
+addappid(2109313)
+addappid(2109313, 1, "5ba6fe4505ceccc8a3944083a0bf83c93f29431035555c3d11fc6d979ea3fafd") -- Resident Evil 4 Charm Handgun Ammo - Main Content
+setManifestid(2109313, "4051934214530803209", 0)
+
+-- Resident Evil 4 Charm Green Herb (AppID: 2109314)
+addappid(2109314)
+addappid(2109314, 1, "06eec89ba4a65d180c33bd61c4ec39f3d0508be30168bda2e522b334cc3395ff") -- Resident Evil 4 Charm Green Herb - Main Content
+setManifestid(2109314, "5791805695380186020", 0)
+
+-- Resident Evil 4 - The Mercenaries (AppID: 2109315)
+addappid(2109315)
+addappid(2109315, 1, "078096d6d8a9b52177c2d9516967c8217edd38adab9a0fa7aef597a6f94d7e92") -- Resident Evil 4 - The Mercenaries - Main Content
+setManifestid(2109315, "1851835095891817569", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (A) (AppID: 2109316)
+addappid(2109316)
+addappid(2109316, 1, "5ed1c39b1d8517a51ed8bbb86229bf6fbabf378c5fc840ac8325e0d6e389afba") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (A) - Main Content
+setManifestid(2109316, "3660849741169791736", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (B) (AppID: 2109317)
+addappid(2109317)
+addappid(2109317, 1, "206ee9679d9ce3b9eaadd476ea046359fe6ad35bc32360a934c0c418f74d766f") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (B) - Main Content
+setManifestid(2109317, "2524887058062613558", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (C) (AppID: 2197320)
+addappid(2197320)
+addappid(2197320, 1, "0999af210483a4459d353d089e0727b1190514eb185300991694b4134ff6e3ed") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (C) - Main Content
+setManifestid(2197320, "3550619730862869173", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (D) (AppID: 2197321)
+addappid(2197321)
+addappid(2197321, 1, "1b3ceddc3b357bd920ff08b1b9e0876fbf0563c0eba014c12303e59ebc1fcad6") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (D) - Main Content
+setManifestid(2197321, "222693115030592431", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (E) (AppID: 2197322)
+addappid(2197322)
+addappid(2197322, 1, "ff522c3cc49b24fd23a6c9d773633f8106a58d651ea388510f30e84d8d5bd81c") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (E) - Main Content
+setManifestid(2197322, "3468692957561704498", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (F) (AppID: 2197323)
+addappid(2197323)
+addappid(2197323, 1, "3a7d63a31ff30659e396bb425ceb400f56f53793c2800609ddbdc086e893c657") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x1 (F) - Main Content
+setManifestid(2197323, "1816669983052022714", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x3 (A) (AppID: 2197324)
+addappid(2197324)
+addappid(2197324, 1, "e3a63d0ad7cfd4b8315137590d5a66ab5fff6bf511fa1a5520df85d8eff2122c") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x3 (A) - Main Content
+setManifestid(2197324, "1058648903756395119", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x3 (B) (AppID: 2197325)
+addappid(2197325)
+addappid(2197325, 1, "39a56b508a0a4b8fa065e2470d9e5451c122a27cddd5a5cf91453caa0e5d2391") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x3 (B) - Main Content
+setManifestid(2197325, "2168598182656536079", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x3 (C) (AppID: 2197326)
+addappid(2197326)
+addappid(2197326, 1, "f18885d2598a61c0a712d6c4452efccbfdcb3358e1054c116bed505004c69898") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x3 (C) - Main Content
+setManifestid(2197326, "630496112153249121", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x3 (D) (AppID: 2197327)
+addappid(2197327)
+addappid(2197327, 1, "f16897d51802fea5d0198c8870cfb0725d3f7bf5341ead388b2bca9e639948ed") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x3 (D) - Main Content
+setManifestid(2197327, "6760043746706895726", 0)
+
+-- Resident Evil 4 Weapon Exclusive Upgrade Ticket x5 (A) (AppID: 2197328)
+addappid(2197328)
+addappid(2197328, 1, "fdd5655f102bf3913333ea1a5aadc4db2f34a1c3c901f3ec0be6549b00982f6f") -- Resident Evil 4 Weapon Exclusive Upgrade Ticket x5 (A) - Main Content
+setManifestid(2197328, "2116811573730420534", 0)
+
+-- DLCS EXCLUDED (NO DEPOT KEYS, NO DEDICATED DEPOTS)
+-- addappid(2593600) -- Resident Evil 4 Original Soundtrack (no keys available)
