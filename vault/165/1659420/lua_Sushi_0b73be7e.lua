18a1a02bba731a8e5aa271bde21d2fb053b2d7ff
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 08:45:55 -0300

    Upload Lote 2 - Prefixo 165

---
+++ vault/165/1659420/lua_Sushi_0b73be7e.lua
@@ -0,0 +1,20 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(1659420) -- UNCHARTED™: Legacy of Thieves Collection
+
+-- MAIN APP DEPOTS
+addappid(1659421, 1, "54f1e37db693643220de1240ebc50a0c00d4bc3a53ffdd91cf3f52cc71c91382") -- Main Game Content (Windows Content)
+setManifestid(1659421, "7819112604911333886", 0)
+addappid(1659422, 1, "513eb93ddc7de41caabd6dfc7f8b2f5da716cc75ca9df1df674e4e339fceada2") -- Game Content (Linux Binaries)
+setManifestid(1659422, "9007195248264946670", 0)
+addappid(1659425, 1, "6bc8b76c2fb8d3e01a2bb8698af8b91196dbcb08fe353fcf64b55649ae766043") -- Main Game Content (Depot 1659425)
+setManifestid(1659425, "5288094124393013920", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
