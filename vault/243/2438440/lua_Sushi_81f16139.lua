63cccbeb940880f8e1f98ee565e4f2340c750c5d
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 10:18:20 -0300

    Upload Lote 2 - Prefixo 243

---
+++ vault/243/2438440/lua_Sushi_81f16139.lua
@@ -0,0 +1,12 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(2438440) -- Hammer of Pain
+
+-- MAIN APP DEPOTS
+addappid(2438441, 1, "f8f676081de0db03b5f01c2c6f6aa76aab9577bfa96d41f88d3f33f432dcaf81") -- Depot 2438441
+setManifestid(2438441, "2540792094253972092", 0)
