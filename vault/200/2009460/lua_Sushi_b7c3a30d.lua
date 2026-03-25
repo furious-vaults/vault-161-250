5772c5097839605969c3779c45e39ce9ffaab4fb
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 09:26:15 -0300

    Upload Lote 2 - Prefixo 200

---
+++ vault/200/2009460/lua_Sushi_b7c3a30d.lua
@@ -0,0 +1,16 @@
+-- Made by @f1uxin, If you would like to check out my discord server here's a link : https://discord.gg/NmmpgnAgen
+-- In my server we help with mani & lua files, fixes, and honestly just anything about piracy,
+-- You can dm me "f1uxin" on discord if you have any questions or anything I'm normally pretty active, and enjoy!
+-- If you want to redistribute/share these files please add and dm "f1uxin" on discord to talk more, me and my friend pay for these games with our own money, we do get games cheaper but its still money that we have and are spending so it sucks when people steal/don't credit me at all.
+
+-- P.S On some socials you may find another person with a close name to me, its "f1uxin sells" or "f1uxins shop" any accounts like that are not mine, i do not sell anything, everything I show is free.
+
+-- P.S. #2. This one is important, you see the "setManifestid" lines below right? If you want your game to "auto update" put "--" without the "" Infront of each "setManifestid" or you can completely remove those lines but that is not recommended, then just save this file, and add it however you add mani & lua files. Example "--setManifestid"
+
+-- Guns.lol link (guns.lol is like linktree, its just my socials) : https://guns.lol/f1uxin
+
+
+-- MAIN APPLICATION 
+addappid(2009460)
+addappid(2009461,0,"05a8e81700e47a3c1ce9bef4fd8c8ad95329d76e93a07bdbaed355bd5d1941a3")
+setManifestid(2009461,"5223287649180371229")
