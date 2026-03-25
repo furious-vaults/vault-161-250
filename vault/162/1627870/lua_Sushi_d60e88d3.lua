92ea1003a13037195c3efb37ead376a9b0437cdf
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 08:43:12 -0300

    Upload Lote 2 - Prefixo 162

---
+++ vault/162/1627870/lua_Sushi_d60e88d3.lua
@@ -0,0 +1,12 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(1627870) -- ODDADA
+
+-- MAIN APP DEPOTS
+addappid(1627871, 1, "5035d70dce2ed8539869a79804512133d229cd722a5107339c5608737a1d448f") -- Main Game Content (Windows Content)
+setManifestid(1627871, "6408425320061413752", 0)
