2d1bc8ee3a6b33e71ca695e036b43d44433b0fa0
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 10:13:10 -0300

    Upload Lote 1 - Prefixo 239

---
+++ vault/239/239030/lua_Sushi_c7e4682d.lua
@@ -0,0 +1,14 @@
+-- Made by f1uxin, please read the read me TXT file.
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(239030) -- Papers, Please
+
+-- MAIN APP DEPOTS
+addappid(239031, 1, "0d3295b4cb4eb5ac0f3bc40478fb875d708b49b80617988ba1751e471bf46253") -- Papers, Please Windows
+setManifestid(239031, "6905656874083880948", 0)
+addappid(239032, 1, "43355768d8e7be51631d0390e4d71e6ac233bd7e2803b10fc2e5c93cae960a88") -- Papers, Please OSX
+setManifestid(239032, "6188834483198805795", 0)
+addappid(239033, 1, "069690842a264d22828b8465895a14b83467482ab765dfcdcb9da6dd25e292b6") -- Papers, Please Linux
+setManifestid(239033, "7577473921595950709", 0)
---
+++ vault/239/2395260/lua_Sushi_5bd802c1.lua
@@ -0,0 +1,10 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(2395260) -- Midnight Shift
+
+-- MAIN APP DEPOTS
+addappid(2395261, 1, "29c2b2e9bb5e0f1fdf802c71052622c6907ff977f461f3768eb8ccfe402fea24") -- Depot 2395261
+setManifestid(2395261, "8731994298596628", 0)
