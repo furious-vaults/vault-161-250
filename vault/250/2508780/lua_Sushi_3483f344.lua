ec13b23cab0a723d0bde6bafcc82b43f2b4c0c9e
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 10:27:21 -0300

    Upload Lote 2 - Prefixo 250

---
+++ vault/250/2508780/lua_Sushi_3483f344.lua
@@ -0,0 +1,30 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪 / username - f1uxin. On discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my (F1uxins) official server: https://discord.gg/NmmpgnAgen
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
+-- MAIN APPLICATION
+addappid(2508780) -- STORY OF SEASONS: Grand Bazaar
+-- MAIN APP DEPOTS
+addappid(2508781, 1, "d3d2cecbf425b32af43928d0b54fb991edc1a74dfac4d72d31a47b22d078ec14") -- Depot 2508781
+setManifestid(2508781, "4760233218950541980", 5049946068)
+-- DLCS WITH DEDICATED DEPOTS
+-- STORY OF SEASONS Grand Bazaar - Cow Set (AppID: 3051830)
+addappid(3051830)
+addappid(3051830, 1, "11061a1ed5036b24ad17351d518ca68411d4fe283cf6156176f5d161b9044ebc") -- STORY OF SEASONS Grand Bazaar - Cow Set - Depot 3051830
+setManifestid(3051830, "7047648226728925275", 1220)
+-- STORY OF SEASONS Grand Bazaar - Trunk of Transformation (AppID: 3248040)
+addappid(3248040)
+addappid(3248040, 1, "d671f9d0718c441e13b212891d9003f5260e69433455f44bcf2c768e61173336") -- STORY OF SEASONS Grand Bazaar - Trunk of Transformation - Depot 3248040
+setManifestid(3248040, "2588098502636584215", 1220)
+-- STORY OF SEASONS Grand Bazaar - Pine Hoverfly Set (AppID: 3482230)
+addappid(3482230)
+addappid(3482230, 1, "0789e54527d096025f42b5c9e7196eed567c1e7b5a83a8466121fe86a3b6a8bb") -- STORY OF SEASONS Grand Bazaar - Pine Hoverfly Set - Depot 3482230
+setManifestid(3482230, "498202292068430120", 1226)
+-- STORY OF SEASONS Grand Bazaar - Art Book (AppID: 3847830)
+addappid(3847830)
+addappid(3847830, 1, "418b5c8fd01c80869d13b37d6ddff62844ed8fa89b0c9c3803a9846c36a96c3f") -- STORY OF SEASONS Grand Bazaar - Art Book - Depot 3847830
+setManifestid(3847830, "726245139049856328", 532210025)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3847810) -- STORY OF SEASONS Grand Bazaar - Super Digital Deluxe DLC Set
