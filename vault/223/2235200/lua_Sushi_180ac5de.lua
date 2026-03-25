3016d29b6a0a473b3ef84659cebb2102c7f4b348
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 09:54:20 -0300

    Upload Lote 2 - Prefixo 223

---
+++ vault/223/2235200/lua_Sushi_180ac5de.lua
@@ -0,0 +1,20 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(2235200) -- Neon Abyss 2
+
+-- MAIN APP DEPOTS
+addappid(2235201, 1, "64b6a7741dff52341709bb548d844f5cbfb4d551a165eba90db90d49ca88586c") -- Depot 2235201
+setManifestid(2235201, "6493388818198407077", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3821980) -- Neon Abyss 2 - Supporter Pack
+
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Neon Abyss 2 Original Soundtrack (AppID: 3821760) - missing depot keys
+-- addappid(3821760)
---
+++ vault/223/223710/lua_Sushi_0a373bcb.lua
@@ -0,0 +1,16 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(223710) -- Cry of Fear
+
+-- MAIN APP DEPOTS
+addappid(223711, 1, "3a01e728a9a3a805348fc7a0fb00a6e22dae50b51c4981f131a54b919e00c8ec") -- Cry of Fear Content
+setManifestid(223711, "3465817078078599269", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") -- .NET 4.0 Redist (Shared from App 228980)
+setManifestid(229002, "7260605429366465749", 0)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") -- .NET 4.0 Client Profile Redist (Shared from App 228980)
+setManifestid(229003, "8740933542064151477", 0)
