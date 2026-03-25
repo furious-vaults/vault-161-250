10d4069005322a8901be1f163a77731beb9a497a
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 10:14:17 -0300

    Upload Lote 1 - Prefixo 240

---
+++ vault/240/2404880/lua_Sushi_a36627e1.lua
@@ -0,0 +1,7 @@
+-- Made By @F1uxin
+-- MAIN APPLICATION
+addappid(2404880) -- Car Dealer Simulator
+
+-- MAIN APP DEPOTS
+addappid(2404881, 1, "dd3322cbddd81741e4bd8f3d9d08fbe6ff172e3f5c36fec42e215f6661d3ea4f") -- Main Game Content (Windows Content)
+setManifestid(2404881, "291469732890498508", 0)
