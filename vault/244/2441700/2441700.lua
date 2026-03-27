18e9fedc25ced49c38d511d63f8e4d2029d21c94
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-27 08:44:22 -0300

    up

---
+++ vault/244/2441700/2441700.lua
@@ -0,0 +1,6 @@
+addappid(2441700)
+addappid(2441701,0,"0d9c8c4dd6db3f468e5e8dde720c2d0842f7daf071cba1a5ad84a8ef3b78b701")
+setManifestid(2441701,"3713374127332855617")
+addappid(3295440)
+addappid(3574310)
+addappid(3207600)
